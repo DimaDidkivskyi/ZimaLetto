bbd3b03dd7a7c24bdaf3c45982271c4665331c9d
YakimovichEV <YakimovichJV@gmail.com>
2021-04-27 14:28:00 +0300

    Added data fetching

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,45 +1,52 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
-
-import productsImg from "../../assets/img/products.png";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export const Products = () => {
+    const { isLoading, error, data, isFetching } = useQuery("repoData", () =>
+        fetch("http://localhost:3000/product").then((res) => res.json())
+    );
+
+    const ProductList = useMemo(() => {
+        if (data) {
+            return data.map((product) => (
+                <div className="products__item">
+                    <Link to="/product" className="products__link">
+                        <div className="products__img">
+                            <img src={product.image} alt="product" />
+                        </div>
+                        <div className="products__title">
+                            <a href="">{product.name}</a>
+                        </div>
+                    </Link>
+                    <p className="products__description">
+                        {product.description}
+                    </p>
+                    <div className="products__rating">
+                        <span className="active"></span>
+                        <span className="active"></span>
+                        <span className="active"></span>
+                        <span className="active"></span>
+                        <span></span>
+                    </div>
+                    <div className="products__price">
+                        <b>{product.price}</b>
+                    </div>
+                    <div className="products__to-cart">
+                        <Link to="/product" className="products__link">
+                            <span>О товаре</span>
+                        </Link>
+                    </div>
+                </div>
+            ));
+        }
+    }, [data]);
+
     return (
         <section className="products">
             <div className="container">
-                <div className="products__inner">
-                    <div className="products__item">
-                        <Link to="/product" className="products__link">
-                            <div className="products__img">
-                                <img src={productsImg} alt="product" />
-                            </div>
-
-                            <div className="products__title">
-                                <a href="">Футболка Ralph Lauren</a>
-                            </div>
-                        </Link>
-                        <p class="products__description">
-                            Эта хлопковая футболка, выстиранная для создания
-                            идеального лаконичного вида, - незаменимая вещь, к
-                            которой вы будете тянуться снова и снова.
-                        </p>
-                        <div className="products__rating">
-                            <span className="active"></span>
-                            <span className="active"></span>
-                            <span className="active"></span>
-                            <span className="active"></span>
-                            <span></span>
-                        </div>
-                        <div className="products__price">
-                            <b>3200 ₽</b>
-                        </div>
-                        <div className="products__to-cart">
-                            <Link to="/product" className="products__link">
-                                <span>О товаре</span>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                <div className="products__inner">{ProductList}</div>
             </div>
         </section>
     );
