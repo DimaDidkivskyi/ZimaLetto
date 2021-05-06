345aefe76d88d08cf26d048a3c61f211426c9d93
Dima Didkivskyi <keenjee90@gmail.com>
2021-05-06 18:28:54 +0300

    Added user reg validation, user log out

---
+++ server/src/validation/user.ts
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export const emailSchema = Joi.object({
+    fname: Joi.string().required(),
+
+    lname: Joi.string(),
+
+    email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+    }),
+
+    password: Joi.string().required(),
+
+    user_role: Joi.string(),
+});
