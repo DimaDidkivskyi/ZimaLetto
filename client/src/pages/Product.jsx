de9ec6fdf2940b4cac603a09ea2c135d478200d1
YakimovichEV <YakimovichJV@gmail.com>
2021-05-06 18:36:12 +0300

    Updated "Product" page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import { ProductInfo } from "../components/Product/ProductInfo";
 import { ProductAbout } from "../components/Product/ProductAbout";
 
 export const Product = () => {
+    let { id } = useParams();
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        const fetchOneProduct = async () => {
+            const resp = await fetch(`http://localhost:3000/api/product/${id}`);
+            const data = await resp.json();
+            setProduct(data);
+            console.log(data);
+        };
+        fetchOneProduct();
+    }, []);
+
     return (
         <div>
-            <ProductInfo />
-            <ProductAbout />
+            <ProductInfo
+                name={product.name}
+                image={product.image}
+                price={product.price}
+            />
+            <ProductAbout description={product.description} />
         </div>
     );
 };
