847d8d85924813baf4f32a7f64e3b73ea794b058
YakimovichEV <YakimovichJV@gmail.com>
2021-05-14 18:14:10 +0300

    Updated actions for redux

--- client/src/redux/actions/clothes.js
+++ client/src/redux/actions/clothes.js
@@ -1,4 +1,28 @@
-const setClothes = (items) => ({
-    type: "SET_CLOTHES",
+import axios from "axios";
+
+export const setLoaded = (payload) => ({
+    type: "SET_LOADED",
+    payload,
+});
+
+export const fetchClothes = (sortBy, category) => (dispatch) => {
+    dispatch({
+        type: "SET_LOADED",
+        payload: false,
+    });
+
+    axios
+        .get(
+            `/products?${
+                category !== null ? `category=${category}` : ""
+            }&_sort=${sortBy.type}&_order=${sortBy.order}`
+        )
+        .then(({ data }) => {
+            dispatch(setPizzas(data));
+        });
+};
+
+export const setPizzas = (items) => ({
+    type: "SET_PIZZAS",
     payload: items,
 });
