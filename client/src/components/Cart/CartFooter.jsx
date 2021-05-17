ca6f8764e157743749ed3768a8547eda69d74d20
YakimovichEV <YakimovichJV@gmail.com>
2021-05-17 21:37:05 +0300

    Edited "Cart" components

--- client/src/components/Cart/CartFooter.jsx
+++ client/src/components/Cart/CartFooter.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const CartFooter = () => {
+    const { totalPrice, totalCount } = useSelector(({ cart }) => cart);
+
     return (
         <div className="cart__footer">
             <div className="left">
                 <p>
-                    Total products: <span>1 pc.</span>{" "}
+                    Total products: <span>{totalCount} pc.</span>{" "}
                 </p>
             </div>
             <div className="right">
                 <p>
-                    Total sum: <span>$1200</span>{" "}
+                    Total sum: <span>{totalPrice}</span>{" "}
                 </p>
             </div>
         </div>
