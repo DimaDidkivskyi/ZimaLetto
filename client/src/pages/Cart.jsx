41e3070eeee3cd98958f113afc69011614993df1
YakimovichEV <YakimovichJV@gmail.com>
2021-05-18 19:21:48 +0300

    Deleted unnecessary files and grouped other

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
-import { CartTitle } from "../components/Cart/CartTitle";
 import { CartItem } from "../components/Cart/CartItem";
-import { CartFooter } from "../components/Cart/CartFooter";
-import { CartBtn } from "../components/Cart/CartBtn";
 import { CartEmpty } from "../components/Cart/CartEmpty";
 
+import imgCart from "../assets/img/cart-cart.svg";
+import imgTrash from "../assets/img/cart-trash.svg";
+import imgArrow from "../assets/img/cart-left-arrow.svg";
+
 export const Cart = () => {
-    const items = useSelector(({ cart }) => cart.items);
+    const dispatch = useDispatch();
+    const { totalPrice, totalCount, items } = useSelector(({ cart }) => cart);
     const addedClothes = Object.keys(items).map((key) => {
         return items[key][0];
     });
@@ -16,15 +18,45 @@ export const Cart = () => {
     return (
         <section className="cart">
             <div className="container">
-                <CartTitle />
+                <div className="cart__title">
+                    <div className="cart-left">
+                        <img src={imgCart} alt="cart" />
+                        <h2>Cart</h2>
+                    </div>
+                    <div className="cart-right">
+                        <img src={imgTrash} alt="" />
+                        <span>Clear cart</span>
+                    </div>
+                </div>
                 <div className="cart__items">
                     {addedClothes.map((obj) => (
-                        <CartItem name={obj.name} size="XL" />
+                        <CartItem name={obj.name} size="XL" totalPrice={}/>
                     ))}
                     {/* <CartItem name="Jersey Crewneck T-Shirt" size="XL" /> */}
                 </div>
-                <CartFooter />
-                <CartBtn />
+                <div className="cart__footer">
+                    <div className="left">
+                        <p>
+                            Total products: <span>{totalCount} pc.</span>{" "}
+                        </p>
+                    </div>
+                    <div className="right">
+                        <p>
+                            Total sum: <span>{totalPrice}</span>{" "}
+                        </p>
+                    </div>
+                </div>
+                <div className="cart__btn">
+                    <div className="left-btn">
+                        <button>
+                            <img src={imgArrow} alt="arrow" />
+                            Come back
+                        </button>
+                    </div>
+                    <div className="right-btn">
+                        <button>Pay now</button>
+                    </div>
+                </div>
                 {/* <CartEmpty /> */}
             </div>
         </section>
--- client/src/redux/reducers/cart.js
+++ client/src/redux/reducers/cart.js
@@ -4,21 +4,26 @@ const initialState = {
     totalCount: 0,
 };
 
+const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0);
+
 const cart = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_PRODUCT_CART": {
+            const currentProductItems = !state.items[action.payload.id]
+                ? [action.payload]
+                : [...state.items[action.payload.id], action.payload];
+
             const newItems = {
                 ...state.items,
-                [action.payload.id]: !state.items[action.payload.id]
-                    ? [action.payload]
-                    : [...state.items[action.payload.id], action.payload],
+                [action.payload.id]: {
+                    items: currentProductItems,
+                    totalPrice: getTotalPrice(currentProductItems),
+                },
             };
 
-            const allProducts = [].concat.apply([], Object.values(newItems));
-            const totalPrice = allProducts.reduce(
-                (sum, obj) => obj.price + sum,
-                0
-            );
+            const items = Object.values(newItems);
+            const allProducts = [].concat.apply([]);
+            const totalPrice = getTotalPrice(allProducts);
 
             return {
                 ...state,
