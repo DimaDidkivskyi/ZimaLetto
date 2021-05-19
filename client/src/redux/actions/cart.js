8d3c2f8d9bfa3051fcc2415b562c58ff623b2def
YakimovichEV <YakimovichJV@gmail.com>
2021-05-19 20:26:45 +0300

    Updated actions

--- client/src/redux/actions/cart.js
+++ client/src/redux/actions/cart.js
@@ -2,3 +2,12 @@ export const addProductCart = (clothesObj) => ({
     type: "ADD_PRODUCT_CART",
     payload: clothesObj,
 });
+
+export const clearCart = () => ({
+    type: "CLEAR_CART",
+});
+
+export const removeCartItem = (id) => ({
+    type: "REMOVE_CART_ITEM",
+    payload: id,
+});
