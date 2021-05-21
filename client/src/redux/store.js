2bb0d7d0895b3a8b572f87d9e6a91de6c603cd50
YakimovichEV <YakimovichJV@gmail.com>
2021-05-21 20:36:31 +0300

    Updated Cart

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,16 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
+const composeEnhancers =
+    (typeof window !== "undefined" &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
