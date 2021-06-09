b5dad18dede0761b87cfa01992ff9355fff5c15f
YakimovichEV <YakimovichJV@gmail.com>
2021-06-09 20:19:35 +0300

    Created adaptive "Header" component

---
+++ client/src/components/Header/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { NavLinks } from "./NavLinks";
+
+export const Navigation = () => {
+    return (
+        <nav className="header__menu">
+            <NavLinks />
+        </nav>
+    );
+};
