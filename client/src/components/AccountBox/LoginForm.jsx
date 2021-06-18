3d02416ade827d1683d2ee6101fb8cd41468a7cb
YakimovichEV <YakimovichJV@gmail.com>
2021-06-18 20:07:38 +0300

    WIP

---
+++ client/src/components/AccountBox/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+
+import { Marginer } from "../marginer/index";
+import {
+    BoxContainer,
+    FormContainer,
+    Input,
+    MutedLink,
+    SubmitButton,
+    BoldLink,
+} from "./common";
+import { AccountContext } from "./accountContext";
+
+export const LoginForm = (props) => {
+    const { switchToSignup } = useContext(AccountContext);
+
+    return (
+        <BoxContainer>
+            <FormContainer>
+                <Input type="email" placeholder="Email" />
+                <Input type="email" placeholder="Email" />
+            </FormContainer>
+            <Marginer direction="vertical" margin={10} />
+            <MutedLink href="#">Forget your password?</MutedLink>
+            <Marginer direction="vertical" margin="1.6em" />
+            <SubmitButton type="submit">Sign in</SubmitButton>
+            <Marginer direction="vertical" margin="1.6em" />
+            <MutedLink href="#">
+                Don't have an account?
+                <BoldLink href="#" onClick={switchToSignup}>
+                    Sign up
+                </BoldLink>{" "}
+            </MutedLink>
+        </BoxContainer>
+    );
+};
