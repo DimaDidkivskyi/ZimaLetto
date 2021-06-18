03a24bf37c3fee85796358424733db5cfd5a84b1
YakimovichEV <YakimovichJV@gmail.com>
2021-06-18 20:07:33 +0300

    WIP

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,7 @@ export { Button } from "./Button";
 export { CartItem } from "./CartItem";
 export { Categories } from "./Categories/Categories";
 export { ContactsAboutUs } from "./ContactsAboutUs";
+export { AccountBox } from "./AccountBox/index";
 export { Footer } from "./Footer";
 export { Header } from "./Header/Header";
 export { HeaderAdmin } from "./HeaderAdmin";
